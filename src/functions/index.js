68fe4f54b598fcc1c848aff4bd77fb75eb05f642
Wei Wang <weiwio@outlook.com>
2019-04-19 15:28:39 -0700

    Optimize functions

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,14 @@
 const functions = require('firebase-functions')
-const indexPage = require('./pages/index')
-const errorPage = require('./pages/_error')
 
-exports.IndexPage = functions.https.onRequest((req, res) => indexPage.render(req, res))
-exports.ErrorPage = functions.https.onRequest((req, res) => errorPage.render(req, res))
+let indexPage
+let errorPage
+
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'IndexPage') {
+  if (!indexPage) indexPage = require('./pages/index')
+  exports['IndexPage'] = functions.https.onRequest((req, res) => indexPage.render(req, res))
+}
+
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'ErrorPage') {
+  if (!errorPage) errorPage = require('./pages/_error')
+  exports['ErrorPage'] = functions.https.onRequest((req, res) => errorPage.render(req, res))
+}
