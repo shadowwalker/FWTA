cb147803154915ea01d38eff688e33edac11e48a
Wei Wang <weiwio@outlook.com>
2019-04-20 21:58:47 -0700

    Update manifest

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -26,7 +26,7 @@ export default class extends Document {
           <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
           <meta name='msapplication-TileColor' content='#2B5797' />
           <meta name='msapplication-tap-highlight' content='no' />
-          <meta name='theme-color' content='#FFFFFF' />
+          <meta name='theme-color' content='#000000' />
           <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
           
           <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
