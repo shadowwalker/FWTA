4844bb09197d6a5fbb92aa55a9d7c8660ca93931
Wei Wang <weiwio@outlook.com>
2019-04-22 20:18:14 -0700

    Add dotenv

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,4 +1,7 @@
+const dotenv = require('dotenv').config()
 const withTypescript = require('@zeit/next-typescript')
+
 module.exports = withTypescript({
+  env: dotenv.error ? {} : dotenv.parsed,
   target: 'serverless'
 })
